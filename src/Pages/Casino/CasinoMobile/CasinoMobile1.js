53a961753fb42025060e484c833107de63c0bb39
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2023-07-06 17:11:33 +0530

    mobile

---
+++ src/Pages/Casino/CasinoMobile/CasinoMobile1.js
@@ -0,0 +1,36 @@
+import "./CasinoMobile1.css";
+const CasinoMobile1 = () => {
+  return (
+    <div className="casinomobile1">
+      <div className="casinomobile1-child" />
+      <b className="online-live-casino">{`Online Live Casino Games Integrations `}</b>
+      <div className="container" />
+      <div className="at-sevens-gaming-container">
+        <p className="at-sevens-gaming">{`At Sevens Gaming, we offer the best in online gaming experiences through our partnerships with the top providers in the industry. `}</p>
+        <p className="at-sevens-gaming">
+          <span>{`Our live casino gaming services provide you with access to the latest and greatest in online gaming experiences. Our platform offers a range of games from `}</span>
+          <b className="blackjack">blackjack</b>
+          <span className="blackjack">{`, `}</span>
+          <b className="blackjack">roulette</b>
+          <span className="blackjack">{`, and `}</span>
+          <b className="blackjack">baccarat</b>
+          <span className="blackjack">{` to newer games like `}</span>
+          <b className="blackjack">Monopoly Live</b>
+          <span className="blackjack">{` and `}</span>
+          <b className="blackjack">Dream Catcher</b>
+          <span className="blackjack">{`. We also offer popular Indian games like `}</span>
+          <b className="blackjack">teen patti</b>
+          <span className="blackjack">{`, `}</span>
+          <b className="blackjack">andar-bahar</b>
+          <span className="blackjack">{`, `}</span>
+          <b className="blackjack">bollywood stars</b>
+          <span className="blackjack">{`, `}</span>
+          <b className="blackjack">jhandi munda</b>
+          <span className="blackjack">{` and much more. Our services are designed to be easy to use and accessible from any device, allowing you to enjoy the gaming experience wherever you are. `}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CasinoMobile1;
---
+++ src/Pages/Homepage/HomepageMobile/HomepageMobile2.jsx
@@ -0,0 +1,42 @@
+import "./HomepageMobile2.css";
+const HomepageMobile2 = () => {
+  return (
+    <div className="homepagesection2mobile">
+      <div className="homepagesection2body">
+        <b className="product-features">Product Features</b>
+        <div className="fe0f-gear-01-03-1-parent">
+          <img
+            className="fe0f-gear-01-03-1-icon"
+            alt=""
+            src="/2699-fe0f-gear-01-03-1@2x.png"
+          />
+          <div className="yellowcards">
+            <div className="yellowcard2">
+              <div className="yellowtextbody">
+                <b className="testimonal-name">IN-PLAY BETTING</b>
+                <div className="testimonial-paragraph">
+                  Better Live Betting Experience
+                </div>
+              </div>
+            </div>
+            <div className="yellowcard2">
+              <div className="yellowtextbody">
+                <b className="testimonal-name">IN-PLAY BETTING</b>
+                <div className="testimonial-paragraph">
+                  Better Live Betting Experience
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="homepagesection2body-child"
+          alt=""
+          src="/group-39469.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomepageMobile2;
