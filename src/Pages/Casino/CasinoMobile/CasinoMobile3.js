c77bdb745d944baa16e8ce575ea7fd2e9e05a484
FaizanShaikh-3033 <faizanvit8@gmail.com>
2023-07-06 17:17:02 +0530

    -

---
+++ src/Pages/Casino/CasinoMobile/CasinoMobile3.js
@@ -0,0 +1,23 @@
+import "./CasinoMobile3.css";
+const CasinoMobile3 = () => {
+  return (
+    <div className="casinomobile3">
+      <div className="casinomobile3-child" />
+      <div className="our-commitment-to-security-parent">
+        <b className="our-commitment-to">{`Our Commitment to Security `}</b>
+        <img
+          className="httpslottiefilescom78078-icon"
+          alt=""
+          src="/httpslottiefilescom78078securitysystem@2x.png"
+        />
+        <div className="we-take-the-container">
+          <p className="we-take-the">{`We take the security of our players very seriously, and we use the latest encryption and security technologies to ensure that our platform is safe and secure. Our live casino games are fair and transparent, and we are committed to providing a responsible gaming environment for our players. We also have a team of dedicated professionals who monitor our platform 24/7 to ensure that our players have a safe and enjoyable gaming experience. `}</p>
+          <p className="we-take-the">&nbsp;</p>
+        </div>
+      </div>
+      <div className="container" />
+    </div>
+  );
+};
+
+export default CasinoMobile3;
