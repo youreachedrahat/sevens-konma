1fb7be1e398e4d9d3f968110ba1c3b6a6e5fa2ce
FaizanShaikh-3033 <faizanvit8@gmail.com>
2023-07-06 17:12:48 +0530

    -

---
+++ src/Pages/Betting/SportsMobile/SportsMobilesection4.js
@@ -0,0 +1,35 @@
+import "./SportsMobilesection4.css";
+const SportsMobilesection4 = () => {
+  return (
+    <div className="sportsmobilesection4">
+      <div className="we-are-committed-to-providing-parent">
+        <div className="we-are-committed-container">
+          <p className="we-are-committed">{`We are committed to providing our clients with a complete suite of solutions to help them achieve their goals and grow their businesses. If you're looking for a reliable and innovative sportsbook software provider, look no further than our Sportsbook and Betting Software Development Company. `}</p>
+          <p className="we-are-committed">&nbsp;</p>
+          <p className="we-are-committed">
+            <b>
+              Contact us today to learn more about how we can help you to take
+              your business to the next level.
+            </b>
+          </p>
+        </div>
+        <div className="master-primary-button">
+          <img
+            className="line-roundedsearch-icon"
+            alt=""
+            src="/line-roundedsearch.svg"
+          />
+          <div className="button-text">Contact Us</div>
+          <img
+            className="line-roundedarrow-right"
+            alt=""
+            src="/line-roundedarrow-right.svg"
+          />
+        </div>
+      </div>
+      <div className="container1" />
+    </div>
+  );
+};
+
+export default SportsMobilesection4;
---
+++ src/Pages/Solutions/BettingMobile/Bettingmobilesection3.js
@@ -0,0 +1,34 @@
+import "./BettingMonilesection3.css";
+const BettingMonilesection3 = () => {
+  return (
+    <div className="bettingmonilesection3">
+      <div className="if-you-are-looking-for-a-relia-parent">
+        <div className="if-you-are-container">
+          <p className="if-you-are">{`If you are looking for a reliable and experienced provider of online sports betting software solutions,  look no further than our company. `}</p>
+          <p className="if-you-are">&nbsp;</p>
+          <p className="if-you-are">
+            <b>
+              Contact us today to learn more about our solutions and how we can
+              help you achieve success in the online sports betting industry.
+            </b>
+          </p>
+        </div>
+        <div className="master-primary-button">
+          <img
+            className="line-roundedsearch-icon"
+            alt=""
+            src="/line-roundedsearch.svg"
+          />
+          <div className="button-text">Contact Us</div>
+          <img
+            className="line-roundedarrow-right"
+            alt=""
+            src="/line-roundedarrow-right.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BettingMonilesection3;
