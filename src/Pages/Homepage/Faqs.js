86b0cb5e64df323b55c0749212612bacfedf76b5
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2023-07-08 22:52:54 +0530

    faq

--- src/Pages/Homepage/Faqs.js
+++ src/Pages/Homepage/Faqs.js
@@ -3,10 +3,10 @@ import BasicAccordion from "../../Components/Accordian";
 const Faqs = () => {
   return (
     <div className="faqs max-sm:w-full  pl-14 max-sm:p-0">
-      <b className="faqs1 max-sm:w-full max-sm:text-[54px] max-sm:text-center text-[72px] max-sm:p-4">FAQs</b>
+      <b className="faqs1 max-sm:w-full max-sm:text-[54px] max-sm:text-center text-[72px] max-sm:p-4 ">FAQs</b>
       <BasicAccordion/>
       <div className="faqImage max-sm:w-full">
-      <img className="faq-icon mt-[-25%] max-sm:w-auto max-sm:h-[220px] max-sm:pb-0" alt="" src="/faq@2x.png" />
+      <img className="faq-icon mt-[-25%] max-sm:w-auto  max-sm:h-[220px] max-sm:pb-0" alt="" src="/faq@2x.png" />
       </div>
     </div>
   );
