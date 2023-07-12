8954e8edb1b5786a2356abe40b6c2f2771dcd112
FaizanShaikh-3033 <faizanvit8@gmail.com>
2023-07-12 12:55:25 +0530

    bettingsection4 button

--- src/Pages/Betting/Bettingsection4.js
+++ src/Pages/Betting/Bettingsection4.js
@@ -4,9 +4,9 @@ import HorizonalRule from "../../Components/HorizontalRule";
 const Bettingsection4 = () => {
   return (
     <div className="bettingsection4 flex flex-row-reverse max-md:flex-col max-sm:bg-primary">
-      <div className="flex flex-col flex-row items-start px-10 py-5 max-sm:px-5 max-sm:text-[16px] w-[60%] max-md:w-[90%] max-sm:gap-5 max-sm:text-white">
+      <div className="flex flex-col flex-row items-start px-10 py-5 max-sm:px-5 max-sm:text-[16px] w-[60%] max-md:w-[90%] max-sm:gap-5 sm:gap-5 max-sm:text-white">
         <div
-          className="lg:mt-12 w-full sm:sleading-10 mt-12 max-sm:text-justify max-sm:leading-8"
+          className="lg:mt-12 w-full sm:sleading-10 mt-12 max-sm:text-justify max-sm:leading-8 sm:leading-8 sm:pt-10"
           style={{ marginTop: "0" }}
         >
           We are committed to providing our clients with a complete suite of
@@ -17,13 +17,13 @@ const Bettingsection4 = () => {
         </div>
         {/*  */}
         <div className="">
-          <b className="textindigobold max-sm:leading-7 w-auto max-sm:h-auto max-sm:text-justify max-sm:tracking-tighter max-sm:text-[16px] max-sm:text-justify max-sm:leading-8">
+          <b className="textindigobold  w-auto max-sm:h-auto  max-sm:tracking-tighter max-sm:text-[16px] max-sm:text-justify max-sm:leading-8">
             Contact us today to learn more about how we can help you to take
             your business to the next level.
           </b>
         </div>
-        <div className="primarybutton max-sm:p-0 max-sm:w-[160px] max-sm:h-[54px] max-sm:mx-auto max-sm:justify-center w-[40%]">
-          <b className="button-text1 max-sm:text-[16px] max-sm:font-semibold max-sm:text-textColor  ">
+        <div className="primarybutton max-sm:p-0 max-sm:w-[160px] max-sm:h-[54px]  max-sm:mx-auto max-sm:justify-center w-[40%] sm:w-[50%] md:w-[50%]  lg:w-[20%] 2xl:w-[20%]   sm:px-0">
+          <b className="button-text1 max-sm:text-[16px] sm:text-center sm:pl-4 max-sm:font-semibold max-sm:text-textColor  ">
             Contact Us
           </b> 
 
